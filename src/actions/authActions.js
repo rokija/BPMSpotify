8ac26f18d9bcfd1c30cce91055f1d7e143e16e9e
rutafreimane <ruta.freimane@hp.com>
2017-11-09 18:34:38 +0200

    added responsive design and changed logic for login flow

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -8,9 +8,9 @@ import { Client } from 'spotify-sdk';
 
 let client = Client.instance;
 
-const client_id = 'fedf859105a2482d8cfb9c2347a9305c';
-const redirect_url = 'http://localhost:3000/callback';
-const scopes = ['user-follow-modify user-follow-read user-library-read user-top-read user-read-private user-read-email'];
+// const client_id = 'fedf859105a2482d8cfb9c2347a9305c';
+// const redirect_url = 'http://localhost:3000/callback';
+// const scopes = ['user-follow-modify user-follow-read user-library-read user-top-read user-read-private user-read-email'];
 
 client.settings = {
     clientId: 'fedf859105a2482d8cfb9c2347a9305c',
@@ -20,38 +20,41 @@ client.settings = {
     redirect_uri: 'http://localhost:3000/callback'
 };
 
-let url = `https://accounts.spotify.com/authorize/?client_id=${client_id}&response_type=token&redirect_uri=${redirect_url}&scope=${scopes}&state=34fFs29kd09&&redirect_uri=${redirect_url}`;
+// let url = `https://accounts.spotify.com/authorize/?client_id=${client_id}&response_type=token&redirect_uri=${redirect_url}&scope=${scopes}&state=34fFs29kd09&&redirect_uri=${redirect_url}`;
+//
+// export function getAuth() {
+//     return function (dispatch) {
+//             axios({
+//                 url: url,
+//                 headers: {
+//                     'Access-Control-Allow-Origin': '*',
+//                 },
+//                 method: 'get'
+//             }).then((response) => {
+//                 dispatch(logInSuccess(response, types.LOG_IN_SUCCESS));
+//                 window.location.href = response.config.url;
+//             }).catch((error) => {
+//                 dispatch(loginError(error, types.LOG_IN_ERROR));
+//             });
+//     };
+// }
+
 
 export function getAuth() {
-    return function (dispatch) {
-            axios({
-                url: url,
-                method: 'get'
+        return function (dispatch) {
+            client.login().then((url) => {
+            window.location.href = url;
+              axios({
+                  url,
+                  method: 'get'
             }).then((response) => {
                 dispatch(logInSuccess(response, types.LOG_IN_SUCCESS));
-                window.location.href = response.config.url;
             }).catch((error) => {
                 dispatch(loginError(error, types.LOG_IN_ERROR));
             });
+        });
     };
 }
-
-
-// export function getAuth() {
-//         return function (dispatch) {
-//             client.login().then((url) => {
-//             window.location.href = url;
-//               axios({
-//                   url,
-//                   method: 'get'
-//             }).then((response) => {
-//                 dispatch(logInSucc(response, types.LOG_IN_SUCCESS));
-//             }).catch((error) => {
-//                 dispatch(Error(error, types.LOG_IN_ERROR));
-//             });
-//         });
-//     };
-// }
 
 
 export function logInSuccess(response) {
@@ -69,7 +72,6 @@ export function loginError(error) {
 }
 
 export function validateCallbackResult(locationHash){
-    console.log(locationHash);
     return (dispatch) => {
         let currentLocationHash = locationHash,
             modifiedHash = currentLocationHash.replace("#",""),
@@ -87,11 +89,7 @@ export function validateCallbackResult(locationHash){
         if(splitHash[0].indexOf("access_token=") !== -1){
             for(let i = 0, ilen = splitHash.length; i < ilen; i++){
                 let splitValue = splitHash[i].split("=");
-                // let expirationTime;
-                // if(splitValue[0] === "expires_in") {
-                //     expirationTime = splitValue[1];
-                //     setTokenDeleteTimeout(expirationTime, dateNow);
-                // }
+
                 if(splitValue[0] === "access_token") {
                     cookies.set("token", splitValue[1], {
                         path: '/',
@@ -108,17 +106,3 @@ export function validateCallbackResult(locationHash){
         }
     };
 }
-
-// export function setTokenDeleteTimeout(expirationTime, dateNow) {
-//     dateNow = dateNow > 0 ? dateNow : Date.now();
-//     let expireTokenAfterMillis = +expirationTime*1000 - dateNow;
-//     // console.log(expireTokenAfterMillis);
-//     let deleteTokenTimeout = setTimeout(() => {
-//         cookies.remove("token", {
-//             path: '/',
-//         });
-//         clearTimeout(deleteTokenTimeout);
-//     }, expireTokenAfterMillis);
-// }
-
-
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
+// import PropTypes from 'prop-types';
+// import Cookies from 'universal-cookie';
+// const cookies = new Cookies();
 
 class HomePage extends Component {
 
-    componentWillMount() {
-        const tokenData = cookies.get('token');
-
-        if (tokenData) {
-            return true;
-        }
-        else {
-            this.context.router.history.push("/login");
-        }
-    }
+    // componentWillMount() {
+    //     const tokenData = cookies.get('token');
+    //
+    //     if (tokenData) {
+    //         return true;
+    //     }
+    //     else {
+    //         this.context.router.history.push("/login");
+    //     }
+    // }
 
     render() {
         return (
             <div className="home-page-wrapper">
                 <div className="link-to-search-page-wrapper">
+                    <h3 className="home-page-title">Get song data from spotify</h3>
                     <Link className="link-to-search-page" to="/search"><span>Enter</span></Link>
                 </div>
             </div>
@@ -29,9 +30,9 @@ class HomePage extends Component {
 }
 
 
-HomePage.contextTypes = {
-    router: PropTypes.object
-};
+// HomePage.contextTypes = {
+//     router: PropTypes.object
+// };
 
 export default HomePage;
 
